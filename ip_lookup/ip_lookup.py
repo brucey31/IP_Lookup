ffd9af55488176710f0cc1b888e8dc3c649a0746
Bruce Pannaman <brucepannaman@Busuus-MacBook-Pro-48.local>
2015-09-10 16:42:05 +0100

    first commit

---
+++ ip_lookup/ip_lookup.py
@@ -0,0 +1,36 @@
+__author__ = 'brucepannaman'
+import csv
+import urllib
+import re
+
+# This script takes a csv file of ip addresses and creates a new file with their locations as the second field
+
+ifile = open('/Users/brucepannaman/Desktop/IP_Lookup_test.csv', "rb")
+reader = csv.reader(ifile, dialect=csv.excel_tab)
+
+new_rows_list = []
+x = 0
+
+for rows in reader:
+    # Save header row.
+    if rows:
+        # print rows[0]
+        # print urllib.urlopen('http://api.hostip.info/get_html.php?ip=%s&position=true' % rows[0]).read()
+        new_row = [rows[0],re.findall("Country:(.*)\\nCity", urllib.urlopen(
+        'http://api.hostip.info/get_html.php?ip=%s&position=true' % rows[0]).read(), re.M | re.I)]
+        if x % 5 == 0:
+            print 'Looked up ' + str(x) + ' IP addresses so far'
+        new_rows_list.append(new_row)
+
+        x += 1
+
+print new_rows_list
+ifile.close()
+
+
+
+# Do the writing
+file2 = open('/Users/brucepannaman/Desktop/IP_Lookup_test_results.csv', 'wb')
+writer = csv.writer(file2)
+writer.writerows(new_rows_list)
+file2.close()
